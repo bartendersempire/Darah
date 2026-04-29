04acd6d8c937efae4e4b79674a0c4e36fca26dd1
K41QU3 <162851465+bartendersempire@users.noreply.github.com>
2026-04-29 00:58:54 -0300

    Update logjs.js

--- site/js/logjs.js
+++ site/js/logjs.js
@@ -1,47 +1,29 @@
-function verificarSenha(event){
-    event.preventDefault(); 
-
-    let senhaDigitada = document.getElementById("senha").value;
-    let senhaCorreta = "15032025";
-
-    if(senhaDigitada == senhaCorreta){
-        document.getElementById("captcha").style.display = "block";
-        document.getElementById("login").style.display = "none";
-        document.getElementById("login2").style.display = "none";
-
-        embaralhar();
-        renderizar();
-    }
-    else{
-        alert("Senha incorreta, errando nossa data de namoro ????? kkkkk");
-    }
-}
-
-
-
-
 let ordem = [0,1,2,3,4,5,6,7,8];
 let selecionado = null;
 
 function embaralhar() {
-    ordem.sort(() => Math.random() - 0.5);
+    for (let i = ordem.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [ordem[i], ordem[j]] = [ordem[j], ordem[i]];
+    }
 }
 
 function renderizar() {
     let grid = document.getElementById("grid");
     grid.innerHTML = "";
 
+    let tamanho = window.innerWidth <= 768 ? 80 : 100;
+
     ordem.forEach((pos, index) => {
         let div = document.createElement("div");
         div.className = "bloco";
 
-        // 👉 Se for o selecionado, adiciona a classe
         if (index === selecionado) {
             div.classList.add("selecionado");
         }
 
-        let x = (pos % 3) * 100;
-        let y = Math.floor(pos / 3) * 100;
+        let x = (pos % 3) * tamanho;
+        let y = Math.floor(pos / 3) * tamanho;
 
         div.style.backgroundPosition = `-${x}px -${y}px`;
 
@@ -59,15 +41,15 @@ function selecionar(index) {
         selecionado = null;
     }
 
-    renderizar(); // 👉 sempre atualiza
+    renderizar();
 }
+
 function verificarCaptcha() {
     let correto = JSON.stringify(ordem) === JSON.stringify([0,1,2,3,4,5,6,7,8]);
 
     if (correto) {
-        window.location.href = "inicio.html"; // 👉 redireciona aqui agora
+        window.location.href = "inicio.html";
     } else {
         alert("Ainda está errado!");
     }
 }
-
